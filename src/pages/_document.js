681da7600d1dea967caabb16503a98fc5ccb4b0c
jmrfG <jorge_ramos13@outlook.com.br>
2023-02-12 13:29:57 -0300

    Solving deployment error: sync req

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,14 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-          integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-          crossOrigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
+
       </Head>
       <body>
         <Main />
