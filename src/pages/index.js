dc72b8c87eb576d7c059cbd5b615d910520ee625
jmrfG <jorge_ramos13@outlook.com.br>
2023-02-16 12:42:59 -0300

    Style refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,7 @@ const postData = async (url = '', data = {}) => {
   }
 };
 
-export default function Home(props) {
-  let data = props.urls
-  console.log(data)
+export default function Home() {
   const [longURL, setLongURL] = useState('');
   const [shortURL, setShortURL] = useState('');
   const [errorString, setErrorString] = useState('');
@@ -28,40 +26,32 @@ export default function Home(props) {
       setShortURL(response.data.shortUrl)
       //      window.location.reload();
     } catch (error) {
-      console.error(error.response.data);
+      window.alert(error.response.data.error)
       setErrorString("You are trying to register a link that already exists in our database.")
     }
   };
 
   return (
-    <div className="container my-4">
+    <div className="container my-5">
       <h1 className="display-3 text-center border-bottom">URL-Shortener</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="url">Original URL</label>
-          <input type="url" className='form-control' value={longURL} onChange={(e) => setLongURL(e.target.value)} />      </div>
-        <br />
-        <div className="form-group">
-          <label htmlFor="url">Short URL</label>
-          {shortURL ? <p><a href={`https://${shortURL}`}>{shortURL}</a></p> : ''}
-          {errorString ? <p>{errorString}</p> : ''}
-        </div>
-        <br />
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-      <small>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group my-3">
+            <label htmlFor="url">Original URL</label>
+            <input type="url" className='form-control' value={longURL} onChange={(e) => setLongURL(e.target.value)} />
+          </div>
+          <div className="form-group my-3">
+            <label htmlFor="surl">Shortened URL</label>
+            <input type="url" className='form-control' value={shortURL} readOnly={true} />
+          </div>
+          <div className='text-center'>
+            <button type="submit" className="btn btn-primary my-3">Submit</button>
+          </div>
+        </form>
+      </div>
+      <p className='text-center'>
         Shortened links will become invalid after 24 hours of its creation.
-      </small>
+      </p>
     </div>
   );
-}
-
-
-export const getStaticProps = async () => {
-  const res = await axios.get("https://zpp.up.railway.app/all")
-  return {
-    props: {
-      urls: res.data
-    }
-  }
 }
